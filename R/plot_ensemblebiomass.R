adcb031b647d9990206f35c33cad16c9267eaa88
Hem Nalini Morzaria-Luna <hmorzaria@hotmail.com>
2022-02-22 23:49:59 +0000

    github action pkgdown

--- R/plot_ensemblebiomass.R
+++ R/plot_ensemblebiomass.R
@@ -13,19 +13,20 @@
 plot_ensemblebiomass <- function(ensemblebiomass, plotmodels){
 
   plot.biomass <- ensemblebiomass %>%
-    filter(longname!="Carrion") %>%
-    filter(!model_ver%in% plotmodels)
+    dplyr::filter(longname!="Carrion") %>%
+    dplyr::filter(!model_ver%in% plotmodels) %>%
+    dplyr::mutate(model_ver = as.factor(model_ver))
 
   thisvariabletype <- "Biomass"
   #library(paletteer)
   #paletteer_d("Redmonder::qMSOSlp")
 
-  col.pal <- redmonder.pal(8, "qMSOSlp")
+  col.pal <- Redmonder::redmonder.pal(8, "qMSOSlp")
 
 
   # Calculate the number of pages with 12 panels per page
   n_pages <- ceiling(
-    length(levels(as.factor(model.biomass.frame$longname)))/ 16
+    length(levels(as.factor(plot.biomass$longname)))/ 16
   )
 
   print(n_pages)
@@ -33,14 +34,14 @@ plot_ensemblebiomass <- function(ensemblebiomass, plotmodels){
 
     print(i)
 
-    pplot <-  ggplot(plot.biomass, aes(x=Year,y=biomass, group = model_ver, colour = model_ver))+
-      geom_line()+
-      labs(y= thisvariabletype, x = "Year") +
-      scale_color_manual(values=col.pal, name = "Model version")+
-      scale_y_continuous(limits = c(0,NA))+
-      facet_wrap_paginate(~longname, ncol = 4, nrow = 4, page = i, shrink = FALSE, scales = "free")+
-      theme_minimal()+
-      theme(strip.text = element_text(size = 7),
+    pplot <-  ggplot2::ggplot(plot.biomass, aes(x=Year,y=biomass, group = model_ver, colour = model_ver))+
+      ggplot2::geom_line()+
+      ggplot2::labs(y= thisvariabletype, x = "Year") +
+      ggplot2::scale_color_manual(values=col.pal, name = "Model version")+
+      ggplot2::scale_y_continuous(limits = c(0,NA))+
+      ggforce::facet_wrap_paginate(~longname, ncol = 4, nrow = 4, page = i, shrink = FALSE, scales = "free")+
+      ggplot2::theme_minimal()+
+      ggplot2::theme(strip.text = element_text(size = 7),
             legend.position="bottom",
             axis.text.x = element_text(size =c(8)),
             axis.text.y = element_text(size =c(8)))
@@ -50,7 +51,7 @@ plot_ensemblebiomass <- function(ensemblebiomass, plotmodels){
 
     thisplotname <- paste(thisvariabletype,i,"model_comparison_plot.png",sep="_")
 
-     ggsave(thisplotname,plot = pplot, device="png",width = 21, height = 29, units = "cm")
+    ggplot2::ggsave(thisplotname,plot = pplot, device="png",width = 21, height = 29, units = "cm")
   }
 
 }
